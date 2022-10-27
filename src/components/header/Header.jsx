bd6c62acec63ba2c3a3162783e380ee63762c18b
Nguyen Thai Hung <hungnt@techasians.com>
2022-10-27 18:01:25 +0700

    header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,43 @@
+import { faBed, faCar, faPlane, faTaxi } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
+import "./header.css"
 
 const Header = () => {
-    return(
-        <div>Header</div>
+    return (
+        <div className="header">
+            <div className="headerContainer">
+                <div className="headerList">
+                    <div className="headerListItem">
+                        <FontAwesomeIcon icon={faBed} />
+                        <span>Stays</span>
+                    </div>
+
+                    <div className="headerListItem">
+                        <FontAwesomeIcon icon={faPlane} />
+                        <span>Flights</span>
+                    </div>
+
+                    <div className="headerListItem">
+                        <FontAwesomeIcon icon={faCar} />
+                        <span>Car rentals</span>
+                    </div>
+
+                    <div className="headerListItem">
+                        <FontAwesomeIcon icon={faBed} />
+                        <span>Attractions</span>
+                    </div>
+
+                    <div className="headerListItem">
+                        <FontAwesomeIcon icon={faTaxi} />
+                        <span>Airport taxis</span>
+                    </div>
+                </div>
+                <h1 className="headerTitle">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+                <p className="headerDesc"> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur, sequi? Temporibus alias sit assumenda</p>
+                <button className="headerButton">Sign in / Register</button>
+            </div>
+        </div>
     )
 }
 
